2ed216e4d24a186f8a0a7782bca9d95fa0d233a0
Harshil-1234 <harshilbansal18@gmail.com>
2025-04-02 22:17:33 +0530

    mongodb connection done

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// require('dotenv').config({path : './env'});
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+/* WHEN PUTTING ALL CODE IN INDEX.JS ONLY */
+// -> Method 1
+// function connectDB(){}
+
+// connectDB()
+
+// -> Method 2 : use IFFY (iffy are starting by using ';' as sometimes if in previous line ';' is missed, it can cause errors)
+// import express from "express"
+// const app = express()
+// ;(async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("App Error: ",error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT , ()=>{
+//             console.log(`App is listening on Port: ${process.env.PORT}`);
+//         })
+//     } catch (error) {
+//         console.error("Error: ",error)
+//         throw err
+//     }
+// })()
